c29b536c175ad2f33af59ccc37bee55e09b36b06
Daniel Koczur <dkoczur@slb.com>
2025-11-15 10:09:10 +0100

    add subdivs

---
+++ Danidrum/ViewModels/SubdivisionViewModel.cs
@@ -0,0 +1,9 @@
+namespace Danidrum.ViewModels;
+
+public class SubdivisionViewModel
+{
+ // Absolute X in pixels
+ public double X { get; set; }
+ public int MeasureIndex { get; set; }
+ public int BeatIndex { get; set; }
+}
